647cb2b26264d1e3eecd76bc15b9e15fa1df603b
Yigit Emecen <ysalih.emecen@gmail.com>
2023-03-16 23:19:38 +0300

    Added crouch functionality
    Player can now crouch. Code also checks for ceilings to prevent player from clipping through them.

--- PlayerController.cs
+++ PlayerController.cs
@@ -6,9 +6,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public bool CanMove = true;
+    private bool IsSprinting => canSprint && Input.GetKey(sprintKey);
+
+    private bool ShouldCrouch => Input.GetKeyDown(crouchKey) && !duringCrouchAnimaiton && characterController.isGrounded;
+
+    [Header("Functional Options")] 
+    [SerializeField] private bool canSprint = true;
+    [SerializeField] private bool canCrouch = true;
+    [SerializeField] private bool canUseHeadbob = true;
+
+    [Header("Controls")] 
+    [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
+    [SerializeField] private KeyCode crouchKey = KeyCode.LeftControl;
 
     [Header("Movement Parameters")] 
     [SerializeField] private float walkSpeed = 3.0f;
+    [SerializeField] private float sprintSpeed = 6.0f;
+    [SerializeField] private float crouchSpeed = 1.5f;
     [SerializeField] private float gravity = 30.0f;
 
     [Header("Look Parameters")] 
@@ -16,6 +30,25 @@ public class PlayerController : MonoBehaviour
     [SerializeField, Range(1, 10)] private float lookSpeedY = 2.0f;
     [SerializeField, Range(1, 180)] private float upperLookLimit = 80.0f;
     [SerializeField, Range(1, 180)] private float lowerLookLimit = 80.0f;
+
+    [Header("Crouch Parameters")] 
+    [SerializeField] private float crouchHeight = 0.5f;
+    [SerializeField] private float standingHeight = 2.0f;
+    [SerializeField] private float timeToCrouch = 0.25f;
+    [SerializeField] private Vector3 crouchingCenter = new Vector3(0, 0.5f, 0);
+    [SerializeField] private Vector3 standingCenter = new Vector3(0, 0, 0);
+    private bool isCrouching;
+    private bool duringCrouchAnimaiton;
+
+    [Header("Headbob Parameters")] 
+    [SerializeField] private float walkBobSpeed = 14f;
+    [SerializeField] private float walkBobAmount = 0.5f;
+    [SerializeField] private float sprintBobSpeed = 20f;
+    [SerializeField] private float sprintBobAmount = 0.8f;
+    [SerializeField] private float crouchBobSpeed = 10f;
+    [SerializeField] private float crouchBobAmount = 0.3f;
+    private float defaultYPos = 0;
+    private float timer;
 
     private Camera playerCamera;
     private CharacterController characterController;
@@ -40,13 +73,18 @@ public class PlayerController : MonoBehaviour
             HandleMovementInput();
             HandleMouseLook();
             
+            if(canCrouch) HandleCrouch();
+            
+            if(canUseHeadbob) HandleHeadbob();
+            
             ApplyFinalMovements();
         }
     }
 
     private void HandleMovementInput()
     {
-        currentInput = new Vector2(walkSpeed * Input.GetAxis("Vertical"), walkSpeed * Input.GetAxis("Horizontal"));
+        currentInput = new Vector2((isCrouching ? crouchSpeed : IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Vertical"), 
+            (isCrouching ? crouchSpeed : IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Horizontal"));
         float moveDirectionY = this.moveDirection.y;
         moveDirection = (transform.TransformDirection(Vector3.forward) * currentInput.x) +
                         (transform.TransformDirection(Vector3.right) * currentInput.y);
@@ -61,11 +99,50 @@ public class PlayerController : MonoBehaviour
         transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeedX, 0);
     }
 
+    private void HandleCrouch()
+    {
+        if (ShouldCrouch) StartCoroutine(CrouchStand());
+    }
+
+    private void HandleHeadbob()
+    {
+        
+    }
+
     private void ApplyFinalMovements()
     {
         if (!characterController.isGrounded) moveDirection.y -= gravity * Time.deltaTime;
 
         characterController.Move(moveDirection * Time.deltaTime);
 
+    }
+
+    private IEnumerator CrouchStand()
+    {
+        if(isCrouching && Physics.Raycast(playerCamera.transform.position, Vector3.up, 1f))
+            yield break;
+        
+        duringCrouchAnimaiton = true;
+
+        float timeElapsed = 0;
+        float targetHeight = isCrouching ? standingHeight : crouchHeight;
+        float currentHeight = characterController.height;
+        Vector3 targetCenter = isCrouching ? standingCenter : crouchingCenter;
+        Vector3 currentCenter = characterController.center;
+
+        while (timeElapsed < timeToCrouch)
+        {
+            characterController.height = Mathf.Lerp(currentHeight, targetHeight, timeElapsed / timeToCrouch);
+            characterController.center = Vector3.Lerp(currentCenter, targetCenter, timeElapsed / timeToCrouch);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        characterController.height = targetHeight;
+        characterController.center = targetCenter;
+
+        isCrouching = !isCrouching;
+        
+        duringCrouchAnimaiton = false;
     }
 }
